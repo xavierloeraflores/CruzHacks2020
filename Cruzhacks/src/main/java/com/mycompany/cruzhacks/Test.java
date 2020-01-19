84d496b199a13ea2d02cf34037265afc30690687
xavierloeraflores <59235228+xavierloeraflores@users.noreply.github.com>
2020-01-18 23:32:20 -0800

    added a test file

---
+++ Cruzhacks/src/main/java/com/mycompany/cruzhacks/Test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cruzhacks;
+
+/**
+ *
+ * @author xavierloera
+ */
+public class Test {
+    public static void main(String args[]){
+        Login ab = new Login();
+        ab.setVisible(true);
+    }
+}
