3d9fc82f4a62fe786e57ed8c65f4d9aafa65587d
xavierloeraflores <59235228+xavierloeraflores@users.noreply.github.com>
2020-01-18 17:03:58 -0800

    Updated Collector file with set and get methods

--- Cruzhacks/src/main/java/com/mycompany/cruzhacks/Collector.java
+++ Cruzhacks/src/main/java/com/mycompany/cruzhacks/Collector.java
@@ -10,10 +10,49 @@ package com.mycompany.cruzhacks;
  * @author xavierloera
  */
 public class Collector extends User{
-    public boolean collectsCardboard;
-    public boolean collectsMetal;
-    public boolean collectsEwaste;
-    public boolean collectsBattery;
-    public boolean collectsWood;
+    private boolean collectsCardboard;
+    private boolean collectsMetal;
+    private boolean collectsEwaste;
+    private boolean collectsBattery;
+    private boolean collectsWood;
+    private Collector(String name, String email, String password, String address,
+            String phone, boolean type){
+        super(name, email, password, address, phone, true);
+        collectsCardboard=false;
+        collectsMetal=false;
+        collectsEwaste=false;
+        collectsBattery=false;
+        collectsWood=false;        
+    }
+    public void setCarboard(boolean change){
+        this.collectsCardboard = change;
+    }
+    public void setMetal(boolean change){
+        this.collectsMetal = change;
+    }
+    public void setEwaste(boolean change){
+        this.collectsEwaste = change;
+    }
+    public void setBattery(boolean change){
+        this.collectsBattery = change;
+    }
+    public void setWood(boolean change){
+        this.collectsWood = change;
+    }
+    public boolean getsCardboard(){
+        return this.collectsCardboard;
+    }
+    public boolean getsMetal(){
+        return this.collectsMetal ;
+    }
+    public boolean getsEwaste(){
+        return this.collectsEwaste ;
+    }
+    public boolean getsBattery(){
+        return this.collectsBattery ;
+    }
+    public boolean getsWood(){
+        return this.collectsWood ;
+    }
     
 }
